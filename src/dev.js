52c7b181e3c5d5aa637d7c76b5d3860bafa5acf2
afeiship <1290657123@qq.com>
2018-06-05 11:15:39 +0800

    issue: update

--- src/dev.js
+++ src/dev.js
@@ -1,6 +1,6 @@
 import './dev.scss';
 import ReactAliOssUpload from './main';
-
+import 'next-guid';
 /*===example start===*/
 
 // install: npm install afeiship/react-ali-oss-upload --save
@@ -8,7 +8,7 @@ import ReactAliOssUpload from './main';
 
 class App extends React.Component{
   state = {
-
+    value:[]
   };
 
   constructor(props){
@@ -19,23 +19,25 @@ class App extends React.Component{
   }
 
   _onChange = e =>{
-    console.log(e);
+    const { value } = e.target;
+    this.setState({ value });
   };
 
   render(){
     const ossToken = {
       "region": "oss-cn-hangzhou",
-      "accessKeyId": "STS.NKJZcdpftTbuHHSonYSXVBieG",
-      "accessKeySecret": "6kv1WyTUAq3ey5zyuQi69h7uDw9TAxaXCePj8VJJJ9HS",
-      "stsToken": "CAISgAJ1q6Ft5B2yfSjIr4j/EdnQnblV46CeSm7ii24MX9d6razOpTz2IHtKeHhgBugasP8xnWlR5voclqYqG8YVaRGVQJFbtswPqVv5lWJbUB3xv9I+k5SANTW5KXyShb3/AYjQSNfaZY3eCTTtnTNyxr3XbCirW0ffX7SClZ9gaKZ8PGD6F00kYu1bPQx/ssQXGGLMPPK2SH7Qj3HXEVBjt3gX6wo9y9zmmZHHu0OC3AOikr9E9t2qGPX+MZkwZqUYesyuwel7epDG1CNt8BVQ/M909vcfp2uW7o7FXwcKvUzfarqIrMcVPg90YKIzHKlft+TxjuFivejeh1U7Q4CtnDg/GoABl7eeLP1wuZmaR167xn0YWgKw7MCfzaIGLHqm2jae3GX3U7gLtywPn/z8pFMQ2XAr8NDxeZlRJnBV2jzmdxrItI9A+1ppTCzWNq1EZgDQjZuz+zbV8W94bbI1LnlqfckqeAxTZosuRIhCqx/dZmg1sTjYlIA9Rw1I+ygesdXRTcc=",
-      "expiration": "2018-05-24 12:51:08",
-      "bucket": "demo-test"
+      "accessKeyId": "STS.NKHopYx2fp3w9K7fRKXPvqnZH",
+      "accessKeySecret": "5RrLtqe9Q1Q5tXtXJ2M72eUna6xFcDasd3KWAfPueLMS",
+      "stsToken": "CAISgAJ1q6Ft5B2yfSjIr4j9JMrtle1Hx/GcO22GglIeVN9anqvxqjz2IHtKeHhgBugasP8xnWlR5voclqYqGsYUb2boNpIgtswPqVv5nH1MPBjxv9I+k5SANTW5KXyShb3/AYjQSNfaZY3eCTTtnTNyxr3XbCirW0ffX7SClZ9gaKZ8PGD6F00kYu1bPQx/ssQXGGLMPPK2SH7Qj3HXEVBjt3gX6wo9y9zmmZHHu0OC3AOikr9E9t2qGPX+MZkwZqUYesyuwel7epDG1CNt8BVQ/M909vcfp2uW7o7FXwcKvUzfarqIrMcVPg90YKIzHKlft+TxjuFivejeh1U7Q4CtnDg/GoABLU/2tQXKtQv8asyhMm1FlGRbqyVTIWVl10mGk9qWx98GHeXh1VWCnMfeV0vLq4Y1W3AgitOtJXvZgqSfZSDhqfdrSlSvjiQyzVxvDWWmWxCfRUC4jpjEdPN5x51AZKuEKSdYxk5N61u2qoRbHKy4BAn+7vExDJ9D//Fd8A+EdUA=",
+      "expiration": "2018-06-05 11:24:18",
+      "bucket": "zhenbao-test"
     };
     return (
       <div className="hello-react-ali-oss-upload">
         <ReactAliOssUpload
           imgServer="http://testimg.treasurevision.cn"
           onChange={this._onChange}
+          filename={nx.guid}
           value={
             [{
               name: 'test',
@@ -43,6 +45,14 @@ class App extends React.Component{
             }]
           }
           ossToken={ossToken} ref='rc' />
+
+          {
+            this.state.value.map(item=>{
+              return (
+                <img src={item.url} alt=""/>
+              )
+            })
+          }
       </div>
     );
   }
